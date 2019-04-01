97d38e8da7eecaa113f1500467ac26cb64c6b554
Easiv <easiv89@gmail.com>
2019-04-01 16:26:42 +0200

    simple response

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 app.get('/', (req, res) => res.send('yo bois'))
+
+app.get('/questions', cors(), (req, res) => {
+  res.send({name: 'Jacek'})
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}.`))
